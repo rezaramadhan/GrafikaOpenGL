4365914110d13f0794c3f7ccf7f1d23a295acaf0
M. Reza Ramadhan <rezaramadhan.m@gmail.com>
2017-04-18 12:54:02 +0700

    Edit pelangi

--- pemandangan.py
+++ pemandangan.py
@@ -1,7 +1,9 @@
+#!/usr/bin/python
+
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
-
+from math import sin, cos, pi
 window = 0                                             # glut window number
 width, height = 824, 700                               # window size
 
@@ -56,8 +58,9 @@ def draw_mountain():
     glVertex2f(280,192);
     glVertex2f(352,134);
     glVertex2f(493,10);
+    glEnd()
 
-    glEnd();
+
 
 def draw_sky():
     glBegin(GL_POLYGON)
@@ -96,94 +99,76 @@ def draw_sky():
     glVertex2f(412,350);
     glEnd();
 
-def draw_rainbow():
+def spectral_color(l):
+    if (l<380.0):
+        r= 0.00
+    elif (l<400.0):
+        r=0.05-0.05*sin(pi*(l-366.0)/ 33.0);
+    elif (l<435.0):
+        r=     0.31*sin(pi*(l-395.0)/ 81.0);
+    elif (l<460.0):
+        r=     0.31*sin(pi*(l-412.0)/ 48.0);
+    elif (l<540.0):
+        r=     0.00;
+    elif (l<590.0):
+        r=     0.99*sin(pi*(l-540.0)/104.0);
+    elif (l<670.0):
+        r=     1.00*sin(pi*(l-507.0)/182.0);
+    elif (l<730.0):
+        r=0.32-0.32*sin(pi*(l-670.0)/128.0);
+    else:
+        r=     0.00
 
-	# glBegin(GL_POLYGON);
-	# glColor3ub(0x00, 0xff, 0x00);
-	# glVertex2f(600,410);
-	# glVertex2f(450,420);
-	# glVertex2f(0,410);
-	# glColor3ub(0x00, 0x00, 0xff);
-	# glVertex2f(0,440);
-	# glVertex2f(450,450);
-	# glVertex2f(600,440);
-	# glEnd();
+    if (l<454.0):
+        g=     0.00;
+    elif (l<617.0):
+        g=     0.78*sin(pi*(l-454.0)/163.0);
+    else:
+        g=     0.00;
 
-	# glBegin(GL_POLYGON);
-	# glColor3ub(0xff, 0xff, 0x00);
-	# glVertex2f(600,380);
-	# glVertex2f(450,390);
-	# glVertex2f(0,380);
-	# glColor3ub(0x00, 0xff, 0x00);
-	# glVertex2f(0,410);
-	# glVertex2f(450,420);
-	# glVertex2f(600,410);
-	# glEnd();
+    if (l<380.0):
+        b=     0.00;
+    elif (l<400.0):
+        b= 0.14-0.14*sin(pi*(l-364.0)/ 35.0);
+    elif (l<445.0):
+        b=     0.96*sin(pi*(l-395.0)/104.0);
+    elif (l<510.0):
+        b=     0.96*sin(pi*(l-377.0)/133.0);
+    else:
+        b=     0.00;
 
-	glBegin(GL_POLYGON);
-	glColor3ub(0x00, 0xff, 0x00);
-	glVertex2f(600,410);
-	glVertex2f(500,420);
-	glVertex2f(400,430);
-	glVertex2f(300,440);
-	glVertex2f(200,430);
-	glVertex2f(100,420);
-	glVertex2f(0,410);
-	glColor3ub(0x00, 0x00, 0xff);
-	glVertex2f(0,440);
-	glVertex2f(100,450);
-	glVertex2f(200,460);
-	glVertex2f(300,470);
-	glVertex2f(400,460);
-	glVertex2f(500,450);
-	glVertex2f(600,440);
-	glEnd();
+    return (r,g,b)
 
-	glBegin(GL_POLYGON);
-	glColor3ub(0xff, 0xff, 0x00);
-	glVertex2f(600,380);
-	glVertex2f(500,390);
-	glVertex2f(400,400);
-	glVertex2f(300,410);
-	glVertex2f(200,400);
-	glVertex2f(100,390);
-	glVertex2f(0,380);
-	glColor3ub(0x00, 0xff, 0x00);
-	glVertex2f(0,410);
-	glVertex2f(100,420);
-	glVertex2f(200,430);
-	glVertex2f(300,440);
-	glVertex2f(400,430);
-	glVertex2f(500,420);
-	glVertex2f(600,410);
-	glEnd();
+def draw_half_circle(x, y, radius, r,g,b):
+    triangleAmount = 100
+    glBegin(GL_TRIANGLE_FAN)
+    glColor3f(r, g, b);
+    glVertex2f(x, y) # center of circle
+    for i in range(0, triangleAmount - 30):
+        glVertex2f(
+            x + (radius * cos(i * 2.0 * pi / triangleAmount)),
+            y + (radius * sin(i * 2.0 * pi / triangleAmount))
+        )
+    glEnd()
 
-	glBegin(GL_POLYGON);
-	glColor3ub(0xff, 0x00, 0x00);
-	glVertex2f(600,350);
-	glVertex2f(500,360);
-	glVertex2f(400,370);
-	glVertex2f(300,380);
-	glVertex2f(200,370);
-	glVertex2f(100,360);
-	glVertex2f(0,350);
-	glColor3ub(0xff, 0xff, 0x00);
-	glVertex2f(0,380);
-	glVertex2f(100,390);
-	glVertex2f(200,400);
-	glVertex2f(300,410);
-	glVertex2f(400,400);
-	glVertex2f(500,390);
-	glVertex2f(600,380);
-	glEnd();
+def draw_rainbow_bagus():
+    x = 650
+    y = 350
+    for i in range(650,410, -2):
+        (r,g,b) = spectral_color(i)
+        draw_half_circle(x,y,i/2,r,g,b)
 
-   
-
-    
-
-
-
-    
+    glBegin(GL_TRIANGLE_FAN)
+    glColor3ub(0xff, 0xcc, 0x00);
+    radius = 200
+    triangleAmount = 100
+    glVertex2f(x, y) # center of circle
+    for i in range(0, triangleAmount + 1):
+        glVertex2f(
+            x + (radius * cos(i * 2.0 * pi / triangleAmount)),
+            y + (radius * sin(i * 2.0 * pi / triangleAmount))
+        )
+    glEnd()
 
 def draw():                                            # ondraw is called all the time
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT) # clear the screen
@@ -192,7 +177,7 @@ def draw():                                            # ondraw is called all th
 
     # draw polygon
     draw_sky()
-    draw_rainbow()
+    draw_rainbow_bagus()
     draw_mountain()
 
     glutSwapBuffers()                                  # important for double buffering
